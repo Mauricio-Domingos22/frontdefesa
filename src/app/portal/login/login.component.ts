8a5b03ec18ddcc5a9eaf06cfdea96305578aba89
aldimiroalfredo <aldimiroalfredo@gmail.com>
2024-03-26 21:25:20 +0100

    this commit fix login by type user

--- src/app/portal/login/login.component.ts
+++ src/app/portal/login/login.component.ts
@@ -14,21 +14,34 @@ export class LoginComponent {
     password: null
   }
 
+  private navigateByUrl: any;
+
   constructor(
     private http: HttpClient,
     private router: Router
-    ) {
-
-  }
+  ) { }
 
   entrar() {
     this.http.post('http://127.0.0.1:3333/login', this.login)
       .subscribe(res => {
 
-        if(res){
-          this.router.navigateByUrl('/area_de_trabalho')
+        console.log(res)
+
+        if (Object(res).user.type_user == "Freelancer") {
+
+          this.navigateByUrl = 'freelance_trabalho'
+
+          this.router.navigateByUrl(`/area_de_trabalho/${this.navigateByUrl}`)
+
+          alert('Seja Bem-Vindo')
+        } else {
+
+          this.navigateByUrl = 'empresa_trabalho'
+
+          this.router.navigateByUrl(`/area_de_trabalho/${this.navigateByUrl}`)
+          alert('Seja Bem-Vindo')
         }
-       })
+      })
   }
 
 
