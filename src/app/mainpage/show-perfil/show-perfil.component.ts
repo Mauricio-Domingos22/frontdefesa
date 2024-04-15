02e4aa7132b744dd1be06990e418de3e68419cc8
aldimiroalfredo <aldimiroalfredo@gmail.com>
2024-04-15 20:26:37 +0100

    this commit getFreelancerPerfil

--- src/app/mainpage/show-perfil/show-perfil.component.ts
+++ src/app/mainpage/show-perfil/show-perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-perfil',
@@ -13,28 +14,50 @@ export class ShowPerfilComponent {
   private token = sessionStorage.getItem('sessionToken');
   headers: HttpHeaders;
 
-  constructor(private http: HttpClient) {
+  user_freelancer: any = []
+  user_frelancer_id: any;
+
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+
     this.headers = new HttpHeaders()
       .set('Content-Type', 'application/json')
       .set('Access-Control-Allow-Origin', '*')
       .set('Authorization', `Bearer ${this.token}`);
+
+
+    this.route.paramMap.subscribe((params) => {
+      this.user_frelancer_id = params.get("user_frelancer_id");
+    });
   }
+
 
   ngOnInit(): void {
     this.getInfoPessoalByUser();
+    this.getFreelancerPerfil()
   }
 
   getInfoPessoalByUser(): void {
     this.http.get<any>('http://127.0.0.1:3333/perfil', { headers: this.headers })
       .subscribe(
         (res: any) => {
-          this.nameUser = res.user.username; 
+          this.nameUser = res.user.username;
           this.specialitys = res.user.especialidade.map((especialidade: any) => especialidade.especialidade).join(', ');
-          console.log('Dados recebidos:', this.nameUser);
         },
         (error) => {
           console.error('Erro ao obter dados do usuário:', error);
         }
       );
+  }
+
+  getFreelancerPerfil() {
+
+    this.http.get('http://127.0.0.1:3333/usuariodados/' + this.user_frelancer_id)
+      .subscribe(response => {
+
+        this.user_freelancer = response
+      })
   }
 }
